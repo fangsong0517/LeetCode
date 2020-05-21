15798300f983f7fcba3062b1dbd76189f052348b
fangsong0517 <2035756541@qq.com>
2020-05-21 19:47:00 +0800

    字符串

---
+++ code/leetcode165.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: leetcode165.cpp
+	> Author:fangsong
+	> Mail: 
+	> Created Time: 2020年05月21日 星期四 18时07分20秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<iomanip>
+#include<algorithm>
+#include<map>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0;
+        while(version1[i] && version2[j]) {
+            int val1 = 0;
+            int val2 = 0;
+            while(version1[i] <= '9' && version1)
+        }
+    }
+};
