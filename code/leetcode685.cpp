752339a2b5c62dbe2a9616031f914f9e9144a6de
fangsong0517 <2035756541@qq.com>
2020-05-23 15:19:10 +0800

    并查集

---
+++ code/leetcode685.cpp
@@ -0,0 +1,88 @@
+/*************************************************************************
+	> File Name: leetcode685.cpp
+	> Author:fangsong
+	> Mail: 
+	> Created Time: 2020年05月23日 星期六 15时18分43秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<iomanip>
+#include<algorithm>
+#include<map>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    struct UnionSet {
+        int *fa, cnt;
+        UnionSet(int n) {
+            fa = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                fa[i] = i;
+            }
+            cnt = n;
+            return ;
+        }
+        int get(int x) {
+            return (fa[x] = (x - fa[x] ? get(fa[x]) : x));
+        }
+        int merge(int a, int b) {
+            if (get(a) == get(b)) return 0;
+            cnt -= 1;
+            fa[get(a)] = get(b);
+            return 1;
+        }
+    };
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int indeg[edges.size() + 1];
+        int outdeg[edges.size() + 1];
+        int fa[edges.size() + 1];
+        int vis[edges.size() + 1];
+        memset(indeg, 0, sizeof(indeg));
+        memset(outdeg, 0, sizeof(outdeg));
+        memset(fa, 0, sizeof(fa));
+        memset(vis, 0, sizeof(vis));
+        int flag = -1;
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            indeg[v] += 1;
+            outdeg[u] += 1;
+            fa[v] = u;
+            if (indeg[v] == 2) flag = v;
+        }
+        if (flag != -1) {
+            for (int i = edges.size() - 1; i >= 0; i--) {
+                if (flag - edges[i][1]) continue;
+                UnionSet u(edges.size());
+                for (int j = 0; j < edges.size(); j++) {
+                    if (i == j) continue;
+                    u.merge(edges[j][0], edges[j][1]);
+                }
+                if (u.cnt != 1) continue;
+                return edges[i];
+            }
+        }
+        queue<int> q;
+        for (int i = 1; i <= edges.size(); i++) {
+            if (outdeg[i] == 0) q.push(i);
+        }
+        while (!q.empty()) {
+            int ind = q.front();
+            vis[ind] = 1;
+            q.pop();
+            outdeg[fa[ind]] -= 1;
+            if (outdeg[fa[ind]] == 0) q.push(fa[ind]);
+        }
+        for (int i = edges.size() - 1; i >= 0; i--) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            if (vis[u] || vis[v]) continue;
+            return edges[i];
+        }
+        return edges[0]; // 没用的
+    }
+};
